d772390ceae30c8f68ec53ab202194cd693478b1
FJG-004 <15028524374@163.com>
2024-08-07 23:21:15 +0800

    Add files via upload

---
+++ test_8_7.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//                                                                     打印100-200中所有素数
+//int su(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j < n; j++)   //2到n-1
+//	{
+//		if (n % j == 0)
+//
+//			return 0;
+//
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		if (su(i) == 1)
+//		{
+//			printf("%d\n", i);
+//			count++;
+//		}
+//	}
+//
+//	printf("count=%d", count);
+//	return 0;
+//}
+int main()
+{
+
+
+
+
+	return 0;
+}
