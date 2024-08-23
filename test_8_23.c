a97fd2ff6ed1cbceb7ff0f9a977d11a701c71e73
FJG-004 <15028524374@163.com>
2024-08-23 22:31:05 +0800

    Add files via upload

---
+++ test_8_23.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");
+//again:
+//	printf("系统将在一分钟后关闭，请输入我是猪，取消关闭\n");
+//	scanf("%s", input);
+//    if(strcmp(input,"我是猪")==0)
+//{
+//	system("shutdown -a");
+//}
+//else
+//{
+//	goto again;
+//}
+//	return 0;
+//}
