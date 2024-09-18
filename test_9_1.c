f78ebd5109795b2b399a5a59f4c60cdff9e78923
FJG-004 <15028524374@163.com>
2024-09-18 21:53:14 +0800

    Add files via upload

---
+++ test_9_1.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//int main()                                      //找不到exe???????????????
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");//cmd
+//	printf("叫爸爸，不然一分钟后关闭你的电脑，点了那个取消也没用哦\n");
+//again:
+//	scanf("%s", input);
+//    if(strcmp(input,"爸爸")==0)
+//{
+//		printf("真乖");
+//	system("shutdown -a");
+//}
+//	else
+//	{
+//		printf("不叫?那就关机喽\n");
+//	goto again;
+//}
+//	return 0;
+//}
