c98cb6b65c5ca553fe7baf6d62f798fd33ec0917
FJG-004 <15028524374@163.com>
2024-09-23 21:56:16 +0800

    Add files via upload

---
+++ homework_9_23.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+	//printf("%.2f", 1.0 / 3);
+	//int k = 3;
+	//printf("%d", sizeof(++k));
+
+	//return 0;
+//}
+//int main()
+//{
+//	float a = 1.123456;
+//	printf("%9.2f", a);
+//
+//	return 0;
+//}
+//int main()
+//{
+//    float a = 2.12;
+//    int b = 88;
+//    char c = 'c';
+//    float d = 4.7;
+//    printf("%c %d %.2f %.2f", c, b, a, d);
+//
+//    return 0;
+//}
+//int main()
+//{
+//	float H = 169;
+//	H = (H - 100) * 0.9 * 2;
+//	printf("%.1f", H);
+//
+//	return 0;
+//}
+#include <stdio.h>
+//int main()
+//{
+//    int a = 988;
+//    float i = 7;
+//    scanf("%d %f", &a, &i);
+//    float c = a * (i / 100);
+//    printf("%.2f", c);
+//    return 0;
+//}
+//int main()
+//{
+//    int A = 5;
+//    int B = 3;
+//    scanf("%d %d", &A, &B);
+//    printf("A + B = %d\n", A + B);
+//    printf("A - B = %d\n", A - B);
+//    printf("A * B = %d\n", A * B);
+//    printf("A / B = %d\n", A / B);
+//    printf("A %% B = %d\n", A % B);
+//
+//    return 0;
+//}
+//int main()
+//{
+//    float t = 3;
+//    float g = 10;
+//    float height = g * t * t / 2;
+//    printf("height = %.2f", height);
+//
+//    return 0;
+//}
+//int main()
+//{
+//	int a = 150;
+//	scanf("%d", &a);
+//	int Celsius = 5 * (a - 32) / 9;
+//	printf("Celsius = %d", Celsius);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int c = 2;
+//	a *= c + 1;
+//	printf("%d", a);
+//
+//	return 0;
+//}
