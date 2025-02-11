c68ececf39ca6c0b0a4d7e888d979769ac9749e0
FJG-004 <15028524374@163.com>
2025-02-11 21:55:59 +0800

    Add files via upload

---
+++ test_2_10.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	char* str1 = "abcdef";
+//	char* str2 = "abcdef";
+//
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abcdef";
+//
+//	if (str1 == str2)
+//	{
+//		printf("str1==str2\n");
+//	}
+//	else
+//	{
+//		printf("str1!=str2\n");
+//	}
+//
+//	if (arr1 == arr2)
+//	{
+//		printf("arr1 == arr2\n");
+//	}
+//	else
+//	{
+//		printf("arr1 != arr2\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//
+//	printf("%p\n", arr);
+//	printf("%p\n\n", arr+1);
+//
+//	printf("%p\n", &arr[0]);
+//	printf("%p\n\n", &arr[0]+1);
+//	
+//	printf("%p\n", &arr);
+//	printf("%p\n\n", &arr+1);
+//
+//
+//	return 0;
+//}
