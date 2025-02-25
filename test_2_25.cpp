8ed36789631ed0db5d1e69cfa1478151c0c1f0ca
FJG-004 <15028524374@163.com>
2025-02-25 22:34:04 +0800

    Add files via upload

---
+++ test_2_25.cpp
@@ -0,0 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+//int main()
+//{
+//	std::cout << "hello world" << std::endl;
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0, b = 0;
+//	std::cin >> a >> b;
+//	std::cout << a * b << std::endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	cout << "/*";//作为单个字符输出
+//	cout << "*/";
+//	cout << /*"*/"*/;
+//	cout << /*"*/"/*"/*"*/;
+//	return 0;
+//}
+
+
+//循环将50加到100
+//int main()
+//{
+//	int sum = 0, num = 50;
+//	while (num <= 100)
+//	{
+//		sum += num;
+//		num++;
+//	}
+//	cout << sum << endl;
+//	return 0;
+//}
+
+
+//循环打印10到0
+//int main()
+//{
+//	unsigned int i = 11;
+//	while (i > 0)
+//	{
+//		i--;
+//		cout << i << endl;
+//	}
+//
+//	return 0;
+//}
+
+//用户输入两个值，屏幕输出两个值之间所有值
+//int main()
+//{
+//	int a = 0, b = 0;
+//	cin >> a >> b;
+//	int temp = a - 1;
+//	while (temp < b)
+//	{
+//		temp++;
+//		cout << temp << ' ' << endl;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int sum = 0;
+//	for (int i = -100; i <= 100; i++)
+//	{
+//		sum += i;
+//	}
+//	cout << sum << endl;
+//	return 0;
+//}
+
+
+//50到100相加
+//int main()
+//{
+//	int sum = 0;
+//	for (int i = 50; i <= 100; i++)
+//	{
+//		sum += i;
+//	}
+//	cout << sum << endl;
+//	return 0;
+//}
+
+
+//10-0打印
+//int main()
+//{
+//	for (int i = 10; i >= 0; i--)
+//	{
+//		cout << i << endl;
+//	}
+//
+//	return 0;
+//}
+
+
+//打印a和b之间所有值
+//int main()
+//{
+//	int a = 0,b = 0;
+//	cin >> a >> b;
+//	for (int temp = a; temp <= b; temp++)
+//	{
+//		cout << temp << endl;
+//	}
+//
+//	return 0;
+//}
