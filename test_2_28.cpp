2d66f4870c69117e004744c9c7b8563623135b37
FJG-004 <15028524374@163.com>
2025-02-28 21:08:36 +0800

    Add files via upload

---
+++ test_2_28.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+//int main()
+//{
+//	int a = 0;
+//	int& pa = a;
+//	pa = 100;
+//	//引用
+//	std::cout << pa << std::endl;
+//	std::cout << a;
+//
+//	return 0;
+//}
+
+//为指针赋值空指针
+//#include <cstdlib>
+//int main()
+//{
+//	int* pa = NULL;//在头文件<cstdlib>定义
+//	int* pb = 0;
+//	int* pc = nullptr;
+//
+//	return 0;
+//}
+
+
+//指针练习
+//using namespace std;
+//int main()
+//{
+//	int a = 6;
+//	int b = 10;
+//	int* pa = &a;
+//	*pa = 60;
+//	cout << a << endl;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b(0);
+//	int c = { 3 };
+//	int d{ 0 };
+//
+//	return 0;
+//}
+
+
+//定义字符串
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string s1(10, 'c');
+//
+//	return 0;
+//}
