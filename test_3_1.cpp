5b3fe003bb4986624a30e7d0e17c91992d1171f8
FJG-004 <15028524374@163.com>
2025-03-01 22:43:38 +0800

    Add files via upload

---
+++ test_3_1.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+// 定义函数模板
+//template <typename T>
+//T add(T a, T b) {
+//    return a + b;
+//}
+//
+//int main() {
+//    int num1 = 3, num2 = 5;
+//    std::cout << "整数相加结果: " << add(num1, num2) << std::endl;
+//
+//    double d1 = 3.5, d2 = 2.5;
+//    std::cout << "浮点数相加结果: " << add(d1, d2) << std::endl;
+//
+//    return 0;
+//}
