238ce8d7336b77f4e43b2243fd492b9e7a37e88f
FJG-004 <15028524374@163.com>
2025-03-13 23:16:56 +0800

    Add files via upload

---
+++ test_3_13.cpp
@@ -0,0 +1,23 @@
+//#include"test.h"
+//using namespace std;
+//int hh, mm;
+//int main()
+//{
+//    cin >> hh >> mm;
+//    if (hh >= 0 && hh < 12)
+//    {
+//        cout << "Only hh:mm.  Too early to Dang.";
+//    }
+//    else if (hh == 12 && mm == 0)
+//    {
+//        cout << "Only hh:mm.  Too early to Dang.";
+//    }
+//    else
+//    {
+//        hh %= 12;
+//        if (mm > 0) hh += 1;
+//        for (int i = 0; i < hh; i++) cout << "Dang";
+//    }
+//
+//    return 0;
+//}
